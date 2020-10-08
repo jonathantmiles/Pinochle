1c1caff3796779298aba513c27acd1b1a6b714b0
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2020-10-08 11:35:59 -0400

    empty project

---
+++ Pinochle/Pinochle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pinochle
+//
+//  Created by Jonathan T. Miles on 10/8/20.
+//  Copyright © 2020 Jonathan T. Miles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
