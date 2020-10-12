26bdd86b3304335ade8d88df45e2a47c89c668d9
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2020-10-12 14:47:37 -0400

    added methods to determine of cards are beatable in a trick setting and if can follow suit; built logic to takeTrick for playing offsuit

--- Pinochle/Pinochle/ModelControllers/HandHandler.swift
+++ Pinochle/Pinochle/ModelControllers/HandHandler.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class HandHandler {
     
-    /// Returns a SortedPile with each suit in its own Pile sorted in descending rank. Suits ordered according to the suitsSorted declaration.
+    ///Returns a SortedPile with each suit in its own Pile sorted in descending rank. Suits ordered according to the suitsSorted declaration.
     func split(hand: Pile) -> SortedPile {
         var sortedHand = Array(repeating: Pile(), count: 4)
         var indexOfSuit = 0
@@ -30,7 +30,7 @@ class HandHandler {
         return sortedHand
     }
     
-    /// Same as 'split(hand:)' except it orders the Trump suit first, and keeps alternating colors according to the suitsSorted declaration.
+    ///Same as 'split(hand:)' except it orders the Trump suit first, and keeps alternating colors according to the suitsSorted declaration.
     func splitPretty(hand: Pile, preferringTrump trump: Character) -> SortedPile {
         var sortedHand = Array(repeating: Pile(), count: 4)
         var indexOfSuit = suitsSorted.firstIndex(of: trump)!
@@ -50,11 +50,50 @@ class HandHandler {
         return sortedHand
     }
     
-    func highestCard(inSuit suit: Character, fromHand hand: SortedPile) -> Int {
-        var indexOfSuit = suitsSorted.firstIndex(of: suit)!
+    ///Takes a normal SortedPile (not prettySplit) and tells whether there are any cards in the suit of the card currenlty winning the trick (assuming it was played legally).
+    func canBeat(cardID: Int, fromSortedHand hand: SortedPile, whereTrump trump: Character) -> Bool {
+        let suitIndex = suitsSorted.firstIndex(of:deck[cardID]!.suit)!
+        let trumpIndex = suitsSorted.firstIndex(of: trump)!
         
+        if hand[suitIndex].isEmpty && hand[trumpIndex].isEmpty {
+            return false
+        } else if !hand[suitIndex].isEmpty && deck[hand[suitIndex].first!]!.value > deck[cardID]!.value {
+            return true
+        } else if suitIndex != trumpIndex {
+            if hand[suitIndex].isEmpty && !hand[trumpIndex].isEmpty {
+                return true
+            }
+        }
         
-        return 0
+        return false
+     }
+    
+    func canFollowSuit(cardID: Int, fromSortedHand hand: SortedPile) -> Bool {
+        let suitIndex = suitsSorted.firstIndex(of:deck[cardID]!.suit)!
+        if hand[suitIndex].isEmpty { return false }
+        return true
+    }
+    // beat with highest card in suit; if no cards in suit, trump low; if no cards in trump, play off suit
+    func takeTrickIfPossible(fromHand hand: SortedPile, towardHighestCard cardID: Int, whereTrump trump: Character) -> Int {
+        let suitIndex = suitsSorted.firstIndex(of:deck[cardID]!.suit)!
+        let trumpIndex = suitsSorted.firstIndex(of: trump)!
+        
+        // if canBeat
+        
+        //play offsuit
+        if !canBeat(cardID: cardID, fromSortedHand: hand, whereTrump: trump) && !canFollowSuit(cardID: cardID, fromSortedHand: hand){
+            var offSuitCandidates: Set<Int> = [0, 1, 2, 3]
+            offSuitCandidates.remove(trumpIndex)
+            offSuitCandidates.remove(suitIndex)
+            
+            for suit in offSuitCandidates {
+                if !hand[suit].isEmpty {
+                    return hand[suit].last!
+                }
+            }
+        }
+        
+        return 52 //hand[indexOfSuit].first
     }
     
 }
